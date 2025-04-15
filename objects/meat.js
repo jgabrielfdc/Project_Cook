23e422dc88f5cc15f9cea08d59573a2a6c499ef3
JGabrielFDC <151442605+jgabrielfdc@users.noreply.github.com>
2025-04-15 12:00:42 -0300

    :construction: Listagem de Pratos

--- objects/meat.js
+++ objects/meat.js
@@ -1,1 +1,9 @@
-alert('olá, mundo')
+const plateList = document.getElementsByClassName('plate__item');
+const plateListSize = plateList.length;
+
+for(let index=0;index<plateListSize;index++){
+	let plateItem=plateList[index];
+	plateItem.addEventListener('click',function(){
+		alert(this.id);
+	});
+}
